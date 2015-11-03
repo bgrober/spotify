328f927a93faf9a488d7d93422d0a218e8766316
Brian <bgrober@gmail.com>
2015-11-02 16:47:45 -0800

    before implementing React

--- src/app.jsx
+++ src/app.jsx
@@ -2,25 +2,29 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-var _ = require('lodash');
-var STRAVA_ACCESS_TOKEN = '85e9e7d17d8d442edb90a8f7c112dfce2de6a7b4';
-var STRAVA_CLIENT_ID = '8506';
-var STRAVA_CLIENT_SECRET = 'e811e15087ce614ae212c85876706235a7bc1d41';
 var ProfileBox = require('./profileBox.jsx');
 var ActivityBox = require('./activityBox.jsx');
+var SignInBox = require('./signin.js');
+var SignInHeader = require('./SignInHeader.js');
 
 var App = React.createClass({
-    render: function() {
-      return (
-        <div >
-          <ProfileBox  url="https://www.strava.com/api/v3/athlete?per_page=1&access_token=85e9e7d17d8d442edb90a8f7c112dfce2de6a7b4&callback=?"/>
-          <ActivityBox url="https://www.strava.com/api/v3/activities?page=2&per_page=200&access_token=85e9e7d17d8d442edb90a8f7c112dfce2de6a7b4&callback=?"/>
-        </div>
-      );
-    }
+  getInitialState: function() {
+    return(
+      username: false,
+
+    );
+  },
+  render: function() {
+    return (
+      <div >
+        <ProfileBox/>
+        <ActivityBox/>
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(
   <App />,
-  document.body
+  document.getElementByID('container');
 );
